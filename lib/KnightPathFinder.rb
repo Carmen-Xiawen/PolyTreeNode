1bc3a7c7bb03808ae5d11e7e769f820119253431
Xiawen Wu (Carmen) <xiawenwuedu@gmail.com>
2019-04-12 16:30:51 -0400

    KMfinder

---
+++ lib/KnightPathFinder.rb
@@ -0,0 +1,15 @@
+class KnightPathFinder
+    def initialize(pos)
+        @considered_positions
+    end
+
+    def self.valid_moves(pos)
+
+
+    end
+    
+    def new_move_positions(pos)
+
+
+    end
+end
